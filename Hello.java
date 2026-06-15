23e98812180ddafbb3e9907020249347a3412d85
Sanket H Fere <154800998+sanketfere@users.noreply.github.com>
2026-06-16 00:28:07 +0530

    last commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 public static void main(String args[]){
   System.out.println("Welcome to Jenkins");
   System.out.println("Added new line to the github repo");
-  System.out.println("Added another new line to the github repo");
+  System.out.println("Added 3rd line to the github repo");
+  System.out.println("Added 4th line to the github repo");
 }
 }
