ec13bb04b75a6718b8ce25d66a0d54aceb2fabf8
Sanket H Fere <154800998+sanketfere@users.noreply.github.com>
2026-06-16 00:26:09 +0530

    added another new line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 public static void main(String args[]){
   System.out.println("Welcome to Jenkins");
   System.out.println("Added new line to the github repo");
+  System.out.println("Added another new line to the github repo");
 }
 }
